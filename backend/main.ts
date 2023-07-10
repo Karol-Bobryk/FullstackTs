285da21a3527e952d7528d59c2ffd68891d5f9cc
Papayusen <KarolekB2002@gmail.com>
2023-07-10 02:07:55 +0200

    Dockerfiles app setup

--- backend/main.ts
+++ backend/main.ts
@@ -2,9 +2,10 @@ import './absolutePath'
 import express, { Express } from 'express'
 import { paths } from './src/const/paths'
 import { serverState } from './src/routes/serverState'
-
+require('dotenv').config()
+const port = process.env.PORT
 const app: Express = express()
 
 app.use(paths.serverState, serverState)
 
-app.listen(5000)
+app.listen(port, () => console.log(`Server is running, port ${port}.`))
